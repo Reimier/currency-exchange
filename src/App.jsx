82491ee84bdf5dc761d9f44a48a84453996f6a46
Rei Seyer <reimierreyes0221@gmail.com>
2025-11-26 16:41:08 +0800

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
           <Routes>
             <Route path="/" element={<HomePage/>} />
             <Route path="/contact" element={<ContactPage/>} />
-            <Route path="*" element={<ExchangeCard/>} />
+            <Route path="*" element={<HomePage/>} />
           </Routes>
         </div>
       </Router>
